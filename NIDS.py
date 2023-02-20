92882d6db87f5f47a5755c934f4872d7f4a8a9c9
JasonZhouE <zhoujason97@gmail.com>
2023-02-19 20:02:51 -0800

    add Jason's part

--- NIDS.py
+++ NIDS.py
@@ -14,18 +14,20 @@ from IPython.core.display import HTML # is used for showing the confusion matrix
 import pydotplus # is used for plotting the decision tree
 from argparse import ArgumentParser
 import pickle
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+import joblib
 
 field_cols = ['srcip','sport','dstip','dsport','proto','state','dur','sbytes','dbytes','sttl','dttl','sloss','dloss','service','Sload','Dload','Spkts','Dpkts','swin','dwin','stcpb','dtcpb','smeansz','dmeansz','trans_depth','res_bdy_len','Sjit','Djit','Stime','Ltime','Sintpkt','Dintpkt','tcprtt','synack','ackdat','is_sm_ips_ports','ct_state_ttl','ct_flw_http_mthd','is_ftp_login','ct_ftp_cmd','ct_srv_src','ct_srv_dst','ct_dst_ltm','ct_src_ ltm','ct_src_dport_ltm','ct_dst_sport_ltm','ct_dst_src_ltm', 'attack_cat', 'Label']
 feature_cols = ['srcip','sport','dstip','dsport','proto','state','dur','sbytes','dbytes','sttl','dttl','sloss','dloss','service','Sload','Dload','Spkts','Dpkts','swin','dwin','stcpb','dtcpb','smeansz','dmeansz','trans_depth','res_bdy_len','Sjit','Djit','Stime','Ltime','Sintpkt','Dintpkt','tcprtt','synack','ackdat','is_sm_ips_ports','ct_state_ttl','ct_flw_http_mthd','is_ftp_login','ct_ftp_cmd','ct_srv_src','ct_srv_dst','ct_dst_ltm','ct_src_ ltm','ct_src_dport_ltm','ct_dst_sport_ltm','ct_dst_src_ltm']
 dtc_cols = ['srcip', 'sport', 'dstip', 'dsport', 'proto', 'sbytes', 'dbytes', 'sttl', 'dttl', 'dloss', 'Sload', 'Dload', 'Spkts', 'Dpkts', 'swin', 'dwin', 'stcpb', 'dtcpb', 'smeansz', 'dmeansz', 'res_bdy_len', 'Sjit', 'Djit', 'Stime', 'Ltime', 'Sintpkt', 'Dintpkt', 'ct_state_ttl', 'ct_srv_src', 'ct_srv_dst', 'ct_dst_ltm', 'ct_src_ ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'ct_dst_src_ltm']
-
+lr_cols = ['dur', 'sbytes', 'sttl', 'dttl', 'Sload', 'Sjit', 'Stime', 'Ltime', 'tcprtt', 'synack', 'ackdat', 'ct_state_ttl', 'ct_flw_http_mthd', 'is_ftp_login', 'ct_srv_src', 'ct_srv_dst', 'ct_dst_ltm', 'ct_src_ ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'ct_dst_src_ltm']
 def main():
     parser = ArgumentParser(
                     prog = 'NIDS AI Program',
                     description = 'This program satisfies the requirements for Project 1 of COMP 8085')
     parser.add_argument('test_filename', help="Path to CSV file containing test data.")
-    parser.add_argument('method', choices=['dtc'], help="The classification method to use for testing the against the dataset.")
+    parser.add_argument('method', choices=['dtc','lr'], help="The classification method to use for testing the against the dataset.")
     parser.add_argument('task', choices=['Label', 'attack_cat'], help="The label to be predicted by the model.")
     parser.add_argument('model_name', help="Optional path to pickled, pre-trained model to load and use for test.")
     parser.add_argument('-fs', choices=['on', 'off'], default="on" ,dest="feature_selection_toggle", help="Optionally turn off feature selection to use all features in dataset.")
@@ -39,6 +41,7 @@ def main():
     traffic.replace(to_replace=r'^\s*$', value=0, inplace=True, regex=True)
     traffic.attack_cat.fillna(value='None', inplace=True)
     traffic.fillna(value=0, inplace=True)
+    traffic = traffic[traffic['is_ftp_login'].isin([0, 1])]
     traffic_features = traffic[feature_cols]
     traffic.attack_cat = traffic.attack_cat.str.strip()
     traffic.attack_cat.replace(to_replace="Backdoors", value="Backdoor", inplace=True)
@@ -61,9 +64,19 @@ def main():
     if args.feature_selection_toggle == 'on':
         if args.method == 'dtc':
             prediction = loaded_model.predict(traffic_features[dtc_cols])
+        if args.method == 'lr':
+            scaler = joblib.load('scaler_model.pkl') 
+            normalized_features = scaler.transform(traffic_features[lr_cols])
+            loaded_model = joblib.load(args.model_name + '.' + args.task + '.sav')        
+            prediction = loaded_model.predict(normalized_features)
     else:
         if args.method == 'dtc':
             prediction = loaded_model.predict(traffic_features)
+        if args.method == 'lr':  
+            scaler = joblib.load('scaler_model.pkl') 
+            normalized_features = scaler.transform(traffic_features)
+            loaded_model = joblib.load(args.model_name + '.' + args.task + '.sav')        
+            prediction = loaded_model.predict(normalized_features)
             
     print(metrics.classification_report(labels[args.task], prediction))
 
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import warnings
 warnings.filterwarnings("ignore")
 import pandas as pd
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
@@ -14,18 +15,22 @@ from IPython.core.display import HTML # is used for showing the confusion matrix
 import pydotplus # is used for plotting the decision tree
 from argparse import ArgumentParser
 import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+import joblib
 
 field_cols = ['srcip','sport','dstip','dsport','proto','state','dur','sbytes','dbytes','sttl','dttl','sloss','dloss','service','Sload','Dload','Spkts','Dpkts','swin','dwin','stcpb','dtcpb','smeansz','dmeansz','trans_depth','res_bdy_len','Sjit','Djit','Stime','Ltime','Sintpkt','Dintpkt','tcprtt','synack','ackdat','is_sm_ips_ports','ct_state_ttl','ct_flw_http_mthd','is_ftp_login','ct_ftp_cmd','ct_srv_src','ct_srv_dst','ct_dst_ltm','ct_src_ ltm','ct_src_dport_ltm','ct_dst_sport_ltm','ct_dst_src_ltm', 'attack_cat', 'Label']
 feature_cols = ['srcip','sport','dstip','dsport','proto','state','dur','sbytes','dbytes','sttl','dttl','sloss','dloss','service','Sload','Dload','Spkts','Dpkts','swin','dwin','stcpb','dtcpb','smeansz','dmeansz','trans_depth','res_bdy_len','Sjit','Djit','Stime','Ltime','Sintpkt','Dintpkt','tcprtt','synack','ackdat','is_sm_ips_ports','ct_state_ttl','ct_flw_http_mthd','is_ftp_login','ct_ftp_cmd','ct_srv_src','ct_srv_dst','ct_dst_ltm','ct_src_ ltm','ct_src_dport_ltm','ct_dst_sport_ltm','ct_dst_src_ltm']
 dtc_cols = ['srcip', 'sport', 'dstip', 'dsport', 'proto', 'sbytes', 'dbytes', 'sttl', 'dttl', 'dloss', 'Sload', 'Dload', 'Spkts', 'Dpkts', 'swin', 'dwin', 'stcpb', 'dtcpb', 'smeansz', 'dmeansz', 'res_bdy_len', 'Sjit', 'Djit', 'Stime', 'Ltime', 'Sintpkt', 'Dintpkt', 'ct_state_ttl', 'ct_srv_src', 'ct_srv_dst', 'ct_dst_ltm', 'ct_src_ ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'ct_dst_src_ltm']
-
+lr_cols = ['dur', 'sbytes', 'sttl', 'dttl', 'Sload', 'Sjit', 'Stime', 'Ltime', 'tcprtt', 'synack', 'ackdat', 'ct_state_ttl', 'ct_flw_http_mthd', 'is_ftp_login', 'ct_srv_src', 'ct_srv_dst', 'ct_dst_ltm', 'ct_src_ ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'ct_dst_src_ltm']
 
 def main():
     parser = ArgumentParser(
                     prog = 'COMP 8085 Project 1 model training program.',
                     description = 'This program trains a model with a given training set and classifier')
     parser.add_argument('train_filename', help="Path to CSV file containing training data.")
-    parser.add_argument('method', choices=['dtc'], help="The classification method to use for testing against the dataset.")
+    parser.add_argument('method', choices=['dtc', 'lr'], help="The classification method to use for testing against the dataset.")
     parser.add_argument('model_name', help="Optional path to pickled, pre-trained model to load and use for test.")
     parser.add_argument('-fs', choices=['on', 'off'], default="on" ,dest="feature_selection_toggle", help="Optionally turn off feature selection to use all features in dataset.")
 
@@ -37,6 +42,7 @@ def main():
     traffic.replace(to_replace=r'^\s*$', value=0, inplace=True, regex=True)
     traffic.attack_cat.fillna(value='None', inplace=True)
     traffic.fillna(value=0, inplace=True)
+    traffic = traffic[traffic['is_ftp_login'].isin([0, 1])]
     traffic_features = traffic[feature_cols]
     traffic.attack_cat = traffic.attack_cat.str.strip()
     traffic.attack_cat.replace(to_replace="Backdoors", value="Backdoor", inplace=True)
@@ -77,8 +83,58 @@ def main():
         filename = args.model_name + '.attack_cat.sav'
         pickle.dump(clf, open(filename, 'wb'))
         
-    elif args.method == 'OTHER CLASSIFIER':
-        # Todo, implement the other classifiers
+    elif args.method == 'lr':
+        # Train the Logistic Regression model
+        lr = LogisticRegression()
+        # Define the parameter grid to search over
+        param_grid = {'C': [0.01], 'penalty': ['l2']}
+
+        # Scaler
+        scaler = StandardScaler()
+        
+        
+        if args.feature_selection_toggle == 'on':
+            # Set up a grid search with cross-validation to find the best hyperparameters
+            gs = GridSearchCV(lr, param_grid, cv=5, n_jobs=-1)
+            # Fit the grid search to the training data
+            X_train = scaler.fit_transform(traffic_features[lr_cols])
+            gs.fit(X_train, label_train)
+            lr = gs.best_estimator_
+        else:
+            # Set up a grid search with cross-validation to find the best hyperparameters
+            gs = GridSearchCV(lr, param_grid, cv=5, n_jobs=-1)
+            # Fit the grid search to the training data
+            X_train = scaler.fit_transform(traffic_features)
+            gs.fit(X_train, label_train)
+            lr = gs.best_estimator_
+        # save the label model to disk
+        filename = args.model_name + '.Label.sav'
+        pickle.dump(lr, open(filename, 'wb'))
+
+        
+        # Set up the logistic regression classifier
+        
+        lr = LogisticRegression()
+        if args.feature_selection_toggle == 'on':
+            # Set up a grid search with cross-validation to find the best hyperparameters
+            gs = GridSearchCV(lr, param_grid, cv=5, n_jobs=-1)
+            # Fit the grid search to the training data
+            X_train = scaler.fit_transform(traffic_features[lr_cols])
+            gs.fit(X_train, attack_cat_train)
+            lr = gs.best_estimator_
+        else:
+            # Set up a grid search with cross-validation to find the best hyperparameters
+            gs = GridSearchCV(lr, param_grid, cv=5, n_jobs=-1)
+            # Fit the grid search to the training data
+            X_train = scaler.fit_transform(traffic_features)
+            gs.fit(X_train, attack_cat_train)
+            lr= gs.best_estimator_
+
+        # save the attack_cat prediction model to disk
+        filename = args.model_name + '.attack_cat.sav'
+        joblib.dump(lr, open(filename, 'wb'))
+        joblib.dump(scaler, 'scaler_model.pkl')
+        
         return
 
 
